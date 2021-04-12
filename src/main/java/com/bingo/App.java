865597697cb83269cede4afbf9f882c4e48e8632
Liviu Varlam <liviu_varlam@yahoo.com>
2021-04-12 11:00:38 +0300

    initial commit

--- src/main/java/com/bingo/App.java
+++ src/main/java/com/bingo/App.java
@@ -4,6 +4,6 @@ public class App {
 
     public static void main(String[] args) {
         TicketGenerator strip = new TicketGenerator();
-        strip.generateTickets(Integer.parseInt("3"));
+        strip.generateTickets(2);
     }
 }
--- src/test/java/TicketGeneratorTest.java
+++ src/test/java/TicketGeneratorTest.java
@@ -11,7 +11,6 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class TicketGeneratorTest {
 
